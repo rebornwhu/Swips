433af93bdcbcf367b2bb2f2caebf534a83bf8e5d
rebornwhu <rebornwhu@gmail.com>
2016-01-12 22:37:29 -0800

    Saving progress

--- Swips/ViewController.swift
+++ Swips/ViewController.swift
@@ -9,15 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    private var gestureStartPoint: CGPoint!
+    
+    private let minimumGestrueLength = Float(5.0)
+    private let maximumVariance = Float(5)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first
+        gestureStartPoint = touch?.locationInView(self.view)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first
+        let currentPosition = touch!.locationInView(self.view)
+        
+        let deltaX = fabsf(Float(gestureStartPoint.x - currentPosition.x))
+        let deltaY = fabsf(Float(gestureStartPoint.y - currentPosition.y))
+        
+        if deltaX >= minimumGestrueLength && deltaY <= maximumVariance {
+            label.text = "Horizontal swipe detected"
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+                self.label.text = ""
+            })
+        }
+        else if deltaY >= minimumGestrueLength && deltaX <= maximumVariance {
+            label.text = "Vertical swipe detected"
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), {
+                self.label.text = ""
+            })
+        }
     }
 
 
